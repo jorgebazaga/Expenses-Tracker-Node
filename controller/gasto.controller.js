2848f9044998579fa909c36c5bd3e70129b97ddb
jorgebazaga <dev.jorgebazaga@gmail.com>
2024-05-14 16:01:05 +0200

    añado obtener gasto por id

--- controller/gasto.controller.js
+++ controller/gasto.controller.js
@@ -23,6 +23,56 @@ async function crearGasto(req, res) {
     }
 }
 
+
+async function obtenerPedidosFarma(req, res) {
+    try {
+      const pedidos = await Pedido.findAll({
+        where: {
+          idFarmacia: req.params.id
+        },
+        order: [['fecha', 'DESC']] 
+      });
+      res.status(200).json({
+        ok: true,
+        status: 200,
+        message: "Pedidos obtenidos correctamente",
+        data: pedidos,
+      });
+    } catch (error) {
+      console.error("Error al obtener los pedidos:", error);
+      res.status(500).json({
+        ok: false,
+        status: 500,
+        message: "Error interno del servidor",
+      });
+    }
+  }
+
+  async function obtenerGastosID(req, res) {
+    try {
+      const gastos = await Gasto.findAll({
+        where: {
+          ID_Usuario: req.params.id
+        },
+        order: [['Fecha', 'DESC']] 
+      });
+      res.status(200).json({
+        ok: true,
+        status: 200,
+        message: "Gastos obtenidos correctamente",
+        data: gastos,
+      });
+    } catch (error) {
+      console.error("Error al obtener los Gastos:", error);
+      res.status(500).json({
+        ok: false,
+        status: 500,
+        message: "Error interno del servidor",
+      });
+    }
+  }
+
 module.exports = {
-    crearGasto
+    crearGasto,
+    obtenerGastosID
 };
--- routes/gasto.routes.js
+++ routes/gasto.routes.js
@@ -5,4 +5,8 @@ router.post("/nuevoGasto", async (req, res) => {
     GastoController.crearGasto(req, res);
 });
 
+router.get("/gastos/:id", async (req, res) => {
+    PedidoController.obtenerPedidosFarma(req, res);
+  });
+
 module.exports = router;
